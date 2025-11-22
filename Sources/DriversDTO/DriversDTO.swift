8e64a879d01963deee55d13e5b628a3c5170b838
ProgrammerTery <terence.changadeya@jamf.com>
2025-11-22 23:54:17 +0200

    Add public initializers to DTO structs
    Added explicit public initializers to DriverDTO, DriverDTOResponseModel, and VehicleDTO to improve instantiation and external usage. Also updated property access levels to public where necessary for consistency.

--- Sources/DriversDTO/DriversDTO.swift
+++ Sources/DriversDTO/DriversDTO.swift
@@ -15,6 +15,13 @@ public struct DriverDTO: Content, Validatable {
     public let password: String
     public let confirmPassword: String
 
+    public init(name: String, email: String, password: String, confirmPassword: String) {
+        self.name = name
+        self.email = email
+        self.password = password
+        self.confirmPassword = confirmPassword
+    }
+
     public static func validations(_ validations: inout Validations) {
         validations.add("name", as: String.self, is: !.empty)
         validations.add("email", as: String.self, is: .email)
@@ -61,8 +68,14 @@ public struct DriverTripRequestPivotDTO: Content {
 }
 
 public struct DriverDTOResponseModel: Content {
-    let name: String
-    let email: String
-    let token: String
+   public let name: String
+   public let email: String
+   public let token: String
+
+    public init(name: String, email: String, token: String) {
+        self.name = name
+        self.email = email
+        self.token = token
+    }
 
 }
--- Sources/DriversDTO/VehicleDTO.swift
+++ Sources/DriversDTO/VehicleDTO.swift
@@ -7,23 +7,45 @@
 import Vapor
 
 struct VehicleDTO: Content {
-    let id : UUID?
-    let registrationNumber: String
-    let licensePlateNumber: String
-    let  make: String
-    let model: String
-    let yearOfManufacture: String
-    let bodyType: String
-    let color: String
-    let engineSize: String
-    let fuelType: String
-    let transmissionType: String
-    let  seatingCapacity: String
-    let ownerName: String
-    let  ownerAddress: String
-    let  contactInformation: String
-    let insuranceDetails: String
-    let vehicleHistory: String
-    let emissionsStandards: String
-    let servicetypeId: UUID
+    public let id : UUID?
+    public let registrationNumber: String
+    public let licensePlateNumber: String
+    public let  make: String
+    public let model: String
+    public let yearOfManufacture: String
+    public let bodyType: String
+    public let color: String
+    public let engineSize: String
+    public let fuelType: String
+    public let transmissionType: String
+    public let  seatingCapacity: String
+    public let ownerName: String
+    public let  ownerAddress: String
+    public let  contactInformation: String
+    public let insuranceDetails: String
+    public let vehicleHistory: String
+    public let emissionsStandards: String
+    public let servicetypeId: UUID
+
+    public init(id: UUID?, registrationNumber: String, licensePlateNumber: String, make: String, model: String, yearOfManufacture: String, bodyType: String, color: String, engineSize: String, fuelType: String, transmissionType: String, seatingCapacity: String, ownerName: String, ownerAddress: String, contactInformation: String, insuranceDetails: String, vehicleHistory: String, emissionsStandards: String, servicetypeId: UUID) {
+        self.id = id
+        self.registrationNumber = registrationNumber
+        self.licensePlateNumber = licensePlateNumber
+        self.make = make
+        self.model = model
+        self.yearOfManufacture = yearOfManufacture
+        self.bodyType = bodyType
+        self.color = color
+        self.engineSize = engineSize
+        self.fuelType = fuelType
+        self.transmissionType = transmissionType
+        self.seatingCapacity = seatingCapacity
+        self.ownerName = ownerName
+        self.ownerAddress = ownerAddress
+        self.contactInformation = contactInformation
+        self.insuranceDetails = insuranceDetails
+        self.vehicleHistory = vehicleHistory
+        self.emissionsStandards = emissionsStandards
+        self.servicetypeId = servicetypeId
+    }
 }
